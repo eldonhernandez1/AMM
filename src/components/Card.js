234a1ad855d3c853672ac14e90a7c03dee16257a
Eldon Hernandez <eldonhernandez@gmail.com>
2023-09-28 20:41:46 -0400

    Making edits to the home page

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,13 @@ import { Card as BootstrapCard } from 'react-bootstrap';
 // Import your images
 import SwapImage from '../images/Swap-01.jpg';
 import WalletImage from '../images/Wallet-01.jpg';
+import CommunityImage from '../images/Community-03.jpg';
 
 function BasicExample() {
   return (
     <Row>
       <Col>
-        <BootstrapCard style={{ width: '18rem' }} className='mx-auto'>
+        <BootstrapCard style={{ width: '18rem' }} className='mx-auto my-4'>
           <BootstrapCard.Img variant="top" src={SwapImage} />
           <BootstrapCard.Body>
             <BootstrapCard.Title>Simple token swapping</BootstrapCard.Title>
@@ -20,7 +21,18 @@ function BasicExample() {
         </BootstrapCard>
       </Col>
       <Col>
-        <BootstrapCard style={{ width: '18rem' }} className='mx-auto'>
+        <BootstrapCard style={{ width: '18rem' }} className='mx-auto my-4'>
+          <BootstrapCard.Img variant="top" src={CommunityImage} />
+          <BootstrapCard.Body>
+            <BootstrapCard.Title>Join the Community</BootstrapCard.Title>
+            <BootstrapCard.Text>
+            Be a part of the vibrant community that drives success by shaping the future.
+            </BootstrapCard.Text>
+          </BootstrapCard.Body>
+        </BootstrapCard>
+      </Col>
+      <Col>
+        <BootstrapCard style={{ width: '18rem' }} className='mx-auto my-4'>
           <BootstrapCard.Img variant="top" src={WalletImage} />
           <BootstrapCard.Body>
             <BootstrapCard.Title>Swap thousands of tokens</BootstrapCard.Title>
