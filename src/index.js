5beb900955cab2d8e68374051e77baa7ae34a804
Eldon Hernandez <eldonhernandez@gmail.com>
2023-11-06 15:01:34 -0500

    Made front end changes

--- src/index.js
+++ src/index.js
@@ -17,6 +17,13 @@ root.render(
   </React.StrictMode>
 );
 
+// // Set the URL to the desired address
+// const newAddress = 'https://blockchain.eldonhernandez.com/KalinaDecentralizedAutonomousOrganization';
+
+// // Use the following code to navigate to the new address
+// window.location.href = newAddress;
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
